81979b70c901275ed207e285b1e44f901c41b32a
BJ Murphy <178152896+BraydonMurphy@users.noreply.github.com>
2024-09-17 05:42:07 -0300

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 document.getElementById("alert-button").addEventListener("click", function() {
     alert("Well Done.");
 });
-// Show a welcome alert when the page loads
-window.onload = function() {
-    alert("Welcome to Braydon's Cool Website! Enjoy your stay!");
-};
